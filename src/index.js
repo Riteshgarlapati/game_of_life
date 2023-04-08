63d2111ce1eb39b1f4f87c29fc9364bf2aa10acb
Riteshgarlapati <riteshgarlapati20@gmail.com>
2023-04-08 12:51:29 +0530

    removed modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
 
 class Box extends React.Component {
 	selectBox = () => {
